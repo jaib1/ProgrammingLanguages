8e39aec6637b78a955923d77352d3bcf49e6a2de
jaib1 <dudecmonitsme@gmail.com>
2018-09-27 18:43:04 -0400

    Finished part1 of HW4

--- PartB/Section5CodeFiles/HW4.rkt
+++ PartB/Section5CodeFiles/HW4.rkt
@@ -6,11 +6,24 @@
 ;; put your code below
 
 #| this makes sure to use TCO for compute optimization
-as opposed to a shorter function |#
+as opposed to a shorter function which doesn't use TCO |#
 
 (define (sequence low high stride)
-  (let helper ([low2 low] [high2 high] [stride2 stride] [acc null])
-    (if (> low2 high2)
-        (reverse acc)
-        (helper (+ low2 stride2) high2 stride2 (cons low2 acc)))))
+  (letrec ([helper (lambda (low2 high2 stride2 acc)
+                     (if (> low2 high2)
+                         (reverse acc)
+                         (helper (+ low2 stride2) high2 stride2 (cons low2 acc))))])
+    (helper low high stride null)))
+
+(define (string-append-map xs suffix)
+  (map (lambda (hdxs) (string-append hdxs suffix)) xs))
+
+(define (list-nth-mod xs n)
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t
+         (let ([i (remainder n (- (length xs) 1))])
+           (car (list-tail xs i)))]))
+        
+
   
